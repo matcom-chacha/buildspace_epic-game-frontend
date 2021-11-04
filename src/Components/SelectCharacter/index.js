cc67e939dcc463ec9fba3268b0ddca9e50435ec0
Gaby <gabybabuchi@gmail.com>
2021-11-03 20:32:19 -0400

    Render  defaults characters.
    Render defaults characters NFTs
    so the user can choose one.

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,10 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
 
-const SelectCharacter = ({ setCharacerNFT }) => {
+const SelectCharacter = ({ setCharacterNFT }) => {
+    const [characters, setCharacters] = useState([]);
+    const [gameContract, setGameContract] = useState([]);
+
+    useEffect(() => {
+        const { ethereum } = window;
+
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const gameContract = new ethers.Contract(
+                CONTRACT_ADDRESS,
+                myEpicGame.abi,
+                signer
+            );
+
+            setGameContract(gameContract);
+        }
+        else {
+            console.log('Ethereum object not found');
+        }
+    }, []);
+
+    useEffect(() => {
+        const getCharacters = async () => {
+            try {
+                console.log('Getting contract characters to mint');
+                //calling contract to get all characters 
+                const charactersTxn = await gameContract.getAllDefaultCharacters();
+                console.log("already made the call");
+                console.log('charactersTxn:', charactersTxn);
+
+                //go through all character and transform the data
+                const characters = charactersTxn.map((characterData) => transformCharacterData(characterData));
+
+                console.log("Characters", characters);
+                setCharacters(characters);
+                console.log(characters.length);
+            }
+            catch (error) {
+                console.log('Something went wrong fetching characters:', error);
+            }
+        };
+
+        if (gameContract) {
+            getCharacters();
+        }
+    }, [gameContract]);
+
+    const renderCharacters = () =>
+        characters.map((character, index) => (
+            <div className="character-item" key={character.name}>
+                <div className="name-container">
+                    <p>{character.name}</p>
+                </div>
+                <img src={character.imageURI} alt={character.name} />
+                <button
+                    type="button"
+                    className="character-mint-button"
+                // onClick={mintCharacterNFTAction(index)}
+                >{`Mint ${character.name}`}</button>
+            </div>
+        ));
+
     return (
         <div className="select-character-container">
             <h2>Mint Your Serial. Choose wisely.</h2>
+            {characters.length > 0 && (
+                <div className="character-grid">{renderCharacters()}</div>
+            )}
         </div>
     );
 };
